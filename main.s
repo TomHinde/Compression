397ff6ca05eb7d8cd4db51e005336b7cf0544ad0
th419 <th419@skbl-422-009.ph.ic.ac.uk>
2021-11-30 11:53:53 +0000

    First commit

---
+++ main.s
@@ -0,0 +1,19 @@
+#include <xc.inc>
+
+psect	code, abs
+	
+main:
+	org	0x0
+	goto	start
+
+	org	0x100		    ; Main code starts here at address 0x100
+start:
+	movlw 	0x0
+	movwf	TRISC, A	    ; Port C all outputs
+	movlw	0x01
+	movwf	PORTC, A
+		    ; Re-run program from start
+
+	end	main
+
+
